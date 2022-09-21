0fd6d1b727dfe27114a510b2522ad70ea0fb8756
jonji <66441843+jonunji@users.noreply.github.com>
2022-09-21 15:54:05 -0400

    Time: 0 ms (100.00%), Space: 6.3 MB (79.96%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -8,7 +8,8 @@ public:
         {
             int dist = s[i] - (stk.empty() ? 0 : stk.top());
             stk.push(s[i]);
-
+            
+            // based on ascii for open and closed paretheses are not far apart             
             if (dist > 0 && dist < 3)
             {
                 stk.pop();
