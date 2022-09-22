34e1cee151ffd66efaddfdf048ad2675cedbfee6
jonji <66441843+jonunji@users.noreply.github.com>
2022-09-22 15:23:57 -0400

    Time: 0 ms (100.00%), Space: 6.2 MB (31.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int climbStairs(int n) 
     {
-        int arr[2] = {1, 2};
+        vector<int> arr(max(n+1, 3));
+        arr[1] = 1;
+        arr[2] = 2;
         
         for (int i = 3; i <= n; i++)
-            arr[(i-1)%2] = arr[i%2] + arr[(i-1)%2];
-        
-        return arr[(n-1)%2];
+            arr[i] = arr[i-1] + arr[i-2];
+    
+        return arr[n];
     }
 };
