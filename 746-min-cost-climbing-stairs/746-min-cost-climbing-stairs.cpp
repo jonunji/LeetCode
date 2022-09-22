d9553e8fcabb9a9b3d5187f0db6be81af13debf3
jonji <66441843+jonunji@users.noreply.github.com>
2022-09-22 15:48:50 -0400

    Time: 0 ms (100.00%), Space: 13.6 MB (96.87%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -11,6 +11,6 @@ public:
         for (int i = 2; i < n; i++)
             arr[i%2] = cost[i] + min(arr[(i-1) % 2], arr[(i-2) % 2]);
         
-        return min(arr[(n - 1) % 2], arr[(n - 2) % 2]);
+        return min(arr[(n - 1) % 2], arr[(n - 2) % 2]); 
     }
 };
