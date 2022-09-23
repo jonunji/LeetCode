43ac427fb81ff77b992447a7521e16693ac7fae8
jonji <66441843+jonunji@users.noreply.github.com>
2022-09-23 14:56:04 -0400

    Time: 45 ms (37.80%), Space: 18.1 MB (63.52%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -2,45 +2,19 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) 
     {
-        set<char> nums;
+        int col[9][9] = {0};
+        int row[9][9] = {0};
+        int box[3][3][9] = {0};
         
-        // check the columns
         for (int i = 0; i < 9; i++)
-        {
             for (int j = 0; j < 9; j++)
-                if (isdigit(board[i][j]) && !nums.insert(board[i][j]).second)
-                    return false;
-            
-            nums.clear();
-        }
-        
-        
-        // check the rows
-        nums.clear();
-        for (int i = 0; i < 9; i++)
-        {
-            for (int j = 0; j < 9; j++)
-                if (isdigit(board[j][i]) && !nums.insert(board[j][i]).second)
-                    return false;
-            
-            nums.clear();
-        }
-        
-        // check the boxes
-        nums.clear();
-        for (int i = 0; i < 9; i++)
-        {
-            for (int j = 0; j < 9; j++)
-            {
-                int row = (i / 3 * 3) + (j / 3);
-                int col = (j % 3) + (i % 3 * 3);
-                
-                if (isdigit(board[row][col]) && !nums.insert(board[row][col]).second)
-                    return false;
-            }
-
-            nums.clear();
-        }
+                if (board[i][j] != '.')
+                {
+                    int num = board[i][j] - '1';
+                    if (row[i][num]++) return false;
+                    if (col[j][num]++) return false;
+                    if (box[i/3][j/3][num]++) return false;
+                }
         
         return true;
     }
