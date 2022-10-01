a46a4d0f8749b0c7d16c137ebebae7de24a2f544
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-01 15:47:51 -0400

    Time: 72 ms (9.25%), Space: 16.4 MB (11.61%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) 
     {
-        vector<vector<int>> res;
         vector<int> curr;
-        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
         
-        combinationSum2(candidates, res, curr, target, 0);
+        backtrack(nums, target, curr, res, 0);
         
         return res;
     }
     
-    void combinationSum2(vector<int> candidates, vector<vector<int>>& res, vector<int> curr, int target, int start) 
+    void backtrack(vector<int>& nums, int target, vector<int> curr, vector<vector<int>>& res, int start)
     {
         if (target == 0)
         {
@@ -19,15 +19,20 @@ public:
             return;
         }
         
-        for (int i = start; i < candidates.size(); i++)
+        // sum is too large
+        if (target < 0) return;
+        
+        for (int i = start; i < nums.size(); i++)
         {
-            if ((target - candidates[i]) >= 0)
-            {
-                if (i > start && candidates[i] == candidates[i-1]) continue;
-                curr.push_back(candidates[i]);
-                combinationSum2(candidates, res, curr, target - candidates[i], i+1);
-                curr.pop_back();
-            }
+            if (i > start && nums[i] == nums[i-1]) continue;
+            
+            curr.push_back(nums[i]);
+            backtrack(nums, target - nums[i], curr, res, i+1);
+            curr.pop_back();
         }
     }
 };
+
+
+// 1 1 2 5 6 7 10
+// 116 125 
