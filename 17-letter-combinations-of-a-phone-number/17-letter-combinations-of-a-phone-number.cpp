d8565e57b92b79060d0fe51ffe3f7a0bd210ca3a
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-01 18:42:29 -0400

    Time: 3 ms (46.77%), Space: 6.5 MB (54.06%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-        vector<string> combos;
-        const vector<string> letters = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
+    vector<vector<char>> dict = {{'a', 'b', 'c'},
+                                 {'d', 'e', 'f'},
+                                 {'g', 'h', 'i'},
+                                 {'j', 'k', 'l'},
+                                 {'m', 'n', 'o'},
+                                 {'p', 'q', 'r', 's'},
+                                 {'t', 'u', 'v'},
+                                 {'w', 'x', 'y', 'z'}};
+    
+    vector<string> letterCombinations(string digits) 
+    {
+        if (digits == "") return {};
         
-        if (digits.empty())
-            return {};
+        vector<string> res;
+        backtrack(digits, res, "", 0);
         
-        combos.push_back("");
-            
-        for (char dig: digits)
+        return res;
+    }
+    
+    void backtrack(string digits, vector<string>& res, string curr, int i)
+    {
+        if (i >= digits.size())
         {
-            vector<string> temp;
-            for (char newLetter: letters[dig - '0'])
-                for (string s: combos)
-                    temp.push_back(s + newLetter);
-            
-            combos.swap(temp);
+            res.push_back(curr);
+            return;
         }
         
-        return combos;
+        int digit = digits[i] - '2';
+        for (char ch : dict[digit])
+        {
+            curr.push_back(ch);
+            backtrack(digits, res, curr, i+1);
+            curr.pop_back();
+        }
     }
 };
