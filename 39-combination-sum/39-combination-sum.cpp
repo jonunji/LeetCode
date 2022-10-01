32575902996c6ae6bed37ffeef217cf074e61715
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-01 15:03:10 -0400

    Time: 78 ms (26.88%), Space: 18.4 MB (31.85%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -11,7 +11,7 @@ public:
         return res;
     }
     
-    void backtrack(vector<int>& nums, int target, int i, vector<int> curr)
+    void backtrack(vector<int>& nums, int target, int start, vector<int> curr)
     {
         if (target == 0)
         {
@@ -19,13 +19,14 @@ public:
             return;
         }
         
-        if (i >= nums.size() || target < 0) return;
+        if (target < 0) return;
         
 
-        curr.push_back(nums[i]);
-        backtrack(nums, target - nums[i], i, curr);
-        curr.pop_back();
-        
-        backtrack(nums, target, i+1, curr);
+        for (int i = start; i < nums.size(); i++)
+        {
+            curr.push_back(nums[i]);
+            backtrack(nums, target - nums[i], i, curr);
+            curr.pop_back();
+        }
     }  
 };
