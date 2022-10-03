08e2a87921f96a73fbeb51d8991dac175ae2f999
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-03 16:03:37 -0400

    Time: 27 ms (80.88%), Space: 13.2 MB (88.87%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pre) 
+    {
+        vector<vector<int>> adjList(n);
+        vector<int> degree(n, 0);
+        
+        for (auto const &p : pre)
+        {
+            degree[p[0]]++;
+            adjList[p[1]].push_back(p[0]);
+        }
+        
+        queue<int> q;
+        
+        for (int i = 0; i < degree.size(); i++)
+            if (degree[i] == 0) q.push(i);
+        
+        while (!q.empty())
+        {
+            int curr = q.front(); q.pop(); n--;
+            
+            for (auto const &v : adjList[curr])
+                if (--degree[v] == 0) q.push(v);
+        }
+            
+        return n == 0;
+    }
+};
