97fa2c1726125514f18fdb25b6e00a5efe3b5203
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-03 16:05:50 -0400

    Time: 37 ms (58.83%), Space: 13.3 MB (83.40%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -5,6 +5,7 @@ public:
         vector<vector<int>> adjList(n);
         vector<int> degree(n, 0);
         
+        // make our adj list and indegrees
         for (auto const &p : pre)
         {
             degree[p[0]]++;
@@ -13,6 +14,7 @@ public:
         
         queue<int> q;
         
+        // find nodes with no incoming edges
         for (int i = 0; i < degree.size(); i++)
             if (degree[i] == 0) q.push(i);
         
@@ -20,6 +22,7 @@ public:
         {
             int curr = q.front(); q.pop(); n--;
             
+            // get rid of one edge going into node, if 0, push to queue
             for (auto const &v : adjList[curr])
                 if (--degree[v] == 0) q.push(v);
         }
