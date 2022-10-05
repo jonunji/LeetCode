8db5bc729a108298ebdb5b00556f160bf3838ebd
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-05 18:03:18 -0400

    Time: 71 ms (43.56%), Space: 12.6 MB (91.84%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,38 +1,66 @@
 class Solution {
 public:
     
-    typedef array<int, 3> dest;
+//     typedef array<int, 3> dest;
+        
+//     Djikstra
+//     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+//     {
+//         // create our adjacency matrix
+//         vector<vector<pair<int, int>>> adj(n);
+//         for (const auto &f : flights)
+//             adj[f[0]].push_back({f[2], f[1]});
+        
+//         // minheap
+//         priority_queue<dest, vector<dest>, greater<dest>> pq;
+//         pq.push({0, src, k+1});
+        
+//         // store our distances to avoid pushing worse options
+//         vector<vector<int>> dist(n, vector<int>(k+2, INT_MAX));
+        
+//         while (!pq.empty())
+//         {
+//             auto [price, val, stops] = pq.top();
+//             pq.pop();
+                        
+            
+//             if (val == dst) return price;
+//             if (!stops) continue;
+            
+//             // check edges of current stop
+//             for (const auto &node : adj[val])
+//                 if (dist[node.second][stops-1] > node.first + price)
+//                 {
+//                     dist[node.second][stops-1] = node.first + price;
+//                     pq.push({node.first + price, node.second, stops - 1});
+//                 }
+//         }
+        
+//         return -1; 
+//     }
     
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+//  Bellman Ford
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k)
     {
-        vector<vector<pair<int, int>>> adj(n);
-        for (const auto &f : flights)
-            adj[f[0]].push_back({f[2], f[1]});
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
         
-        priority_queue<dest, vector<dest>, greater<dest>> pq;
-        pq.push({0, src, k+1});
-        
-        // store our distances to avoid pushing worse options
-        vector<vector<int>> dist(n, vector<int>(k+2, INT_MAX));
-        
-        while (!pq.empty())
+        // < OR = ?????
+        for (int i = 0; i <= k; i++)
         {
-            auto [price, val, stops] = pq.top();
-            pq.pop();
+            vector<int> temp(dist);
             
-            // cout << price << "  " << val << "  " << stops << endl;
+            for (const vector<int> &f : flights)
+            {
+                int curr = f[0], next = f[1], price = f[2];
+                
+                if (dist[curr] == INT_MAX) continue;
+                temp[next] = min(dist[curr] + price, temp[next]);
+            }
             
-            if (val == dst) return price;
-            if (!stops) continue;
-            
-            for (const auto &node : adj[val])
-                if (dist[node.second][stops-1] > node.first + price)
-                {
-                    dist[node.second][stops-1] = node.first + price;
-                    pq.push({node.first + price, node.second, stops - 1});
-                }
+            dist = temp;
         }
         
-        return -1; 
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
     }
 };
