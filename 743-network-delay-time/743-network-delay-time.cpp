5f7cc4bbcb7ead541fce87f2d09992cad3bbfbec
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-05 14:40:47 -0400

    Time: 106 ms (99.66%), Space: 41.1 MB (51.02%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
+    
+    typedef pair<int, int> time;
+    
+    // dijktras
     int networkDelayTime(vector<vector<int>>& times, int n, int k) 
     {     
-        int res;
-        vector<bool> visited(n+1, false);
-        priority_queue<pair<int, int>> pq;
+        vector<vector<time>> adj(n+1);
+        for (auto const &t : times)
+            adj[t[0]].push_back({t[1], t[2]});
         
-        pq.push({0, k});
+        int res;
+        vector<bool> visited(n+1, false); // n+1 since values start at 1
+        priority_queue<time, vector<time>, greater<time>> pq;
+        
+        pq.push({0, k}); // start with our k
         
         while (!pq.empty())
         {
-            int currDist = -pq.top().first;
+            // using negatives cuz of maxheap instead of minheap
+            int currDist = pq.top().first;
             int currVal = pq.top().second;
             pq.pop();
             
+            // skip if we have seen this before
             if (visited[currVal]) continue;
             visited[currVal] = true;
             
+            // currDist will always be the highest, lowest number in our final distances
             res = currDist;
             if (--n == 0) return res;
             
-            for (auto const &time : times)
-                if (!visited[time[1]] && time[0] == currVal)
-                    pq.push({-(currDist + time[2]), time[1]});
+            for (auto const &t : adj[currVal])
+                if (!visited[t.first])
+                    pq.push({currDist + t.second, t.first});
         }
         
         
