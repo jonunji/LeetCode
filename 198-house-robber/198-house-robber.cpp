5bf1da5fe3ced14169a05d3f9a9515c7753eae36
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-05 18:32:19 -0400

    Time: 0 ms (100.00%), Space: 7.7 MB (77.59%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -22,17 +22,31 @@ public:
 //     }
     
     
+//     int rob(vector<int>& nums) 
+//     {
+//         int n = nums.size();
+//         vector<int> dp(n+1);
+        
+//         dp[0] = 0;
+//         dp[1] = nums[0];
+        
+//         for (int i = 1; i < n; i++)
+//             dp[i+1] = max(dp[i-1] + nums[i], dp[i]);
+        
+//         return dp[n];
+//     }
+    
     int rob(vector<int>& nums) 
     {
         int n = nums.size();
-        vector<int> dp(n+1);
+        int dp[2];
         
         dp[0] = 0;
         dp[1] = nums[0];
         
         for (int i = 1; i < n; i++)
-            dp[i+1] = max(dp[i-1] + nums[i], dp[i]);
+            dp[(i+1)%2] = max(dp[(i-1)%2] + nums[i], dp[i%2]);
         
-        return dp[n];
+        return dp[n%2];
     }
 };
