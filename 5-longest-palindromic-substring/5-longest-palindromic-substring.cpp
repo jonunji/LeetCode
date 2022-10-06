81c8a5622c0a205773cc4b911f3148716c009f5b
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-05 22:09:53 -0400

    Time: 181 ms (47.68%), Space: 159.5 MB (26.77%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,35 +1,64 @@
 class Solution {
 public:
+//     DP solution
+//     string longestPalindrome(string s) 
+//     {
+//         int n = s.size();
+//         int left = 0, right = 0;
+        
+//         vector<vector<bool>> dp(n, vector<bool>(n));
+//         for (int i = 0; i < n; i++) 
+//         {
+//             // sets up diagonal
+//             dp[i][i] = true;
+            
+//             // sets up two letters
+//             if (i < n-1) 
+//             {
+//                 dp[i][i+1] = s[i] == s[i+1];
+                
+//                 if (dp[i][i+1]) 
+//                     right = i+1, left = i;
+//             }
+//         }
+        
+//         for (int i = n-3; i >= 0; i--)
+//             for (int j = i+2; j < n; j++)
+//             {
+//                 dp[i][j] = dp[i+1][j-1] && s[i] == s[j];
+                
+//                 if (dp[i][j] && j - i > right - left) 
+//                     right = j, left = i;
+//             }
+        
+//         return s.substr(left, right-left+1);
+//     }
+    
+    int maxLen = 0, start = 0;
+    
     string longestPalindrome(string s) 
     {
-        int n = s.size();
-        int left = 0, right = 0;
+        int len = s.length();
+        if (len < 2) return s;
         
-        vector<vector<bool>> dp(n, vector<bool>(n));
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < len-1; i++)
         {
-            // sets up diagonal
-            dp[i][i] = true;
-            
-            // sets up two letters
-            if (i < n-1) 
-            {
-                dp[i][i+1] = s[i] == s[i+1];
-                
-                if (dp[i][i+1]) 
-                    right = i+1, left = i;
-            }
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i+1);
         }
         
-        for (int i = n-3; i >= 0; i--)
-            for (int j = i+2; j < n; j++)
-            {
-                dp[i][j] = dp[i+1][j-1] && s[i] == s[j];
-                
-                if (dp[i][j] && j - i > right - left) 
-                    right = j, left = i;
-            }
+        return s.substr(start, maxLen);
+    }
+    
+    void extendPalindrome(string s, int i, int j)
+    {
+        while (i >= 0 && j < s.length() && s[i] == s[j]) 
+            i--, j++;
         
-        return s.substr(left, right-left+1);
+        if (j - i - 1 > maxLen) 
+        {
+            start = i + 1;
+            maxLen = j - i - 1;
+        }
     }
 };
