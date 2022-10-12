d41ca7f645a96a75ddf983f352030c345816000b
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-12 12:30:36 -0400

    Time: 3 ms (45.97%), Space: 5.8 MB (91.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int uniquePaths(int m, int n) 
     {
-        int* dp[m];
-        for (int i = 0; i < m; i++)
-            dp[i] = (int*) malloc(n * sizeof(int));
+        int dp[m][n];
         
         for (int i = 0; i < m; i++) dp[i][0] = 1;
         for (int i = 1; i < n; i++) dp[0][i] = 1;
