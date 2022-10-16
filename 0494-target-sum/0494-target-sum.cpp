5ceecc4b1b018646a5356d854bc320b8877e04c8
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-15 20:39:59 -0400

    Time: 44 ms (51.83%), Space: 9.3 MB (62.47%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) 
+    {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (abs(target) > sum) return 0;
+        
+        int n = nums.size(), m = 2 * sum + 1;        
+        int dp[m][n];
+        
+        for (int i = 0; i < m; i++)
+        {
+            dp[i][0] = 0;
+            
+            dp[i][0] += i-sum == -nums[0] ? 1 : 0;
+            dp[i][0] += i-sum == nums[0] ? 1 : 0;
+        }
+        
+        for (int j = 1; j < n; j++)
+            for (int i = -sum; i <= sum; i++)
+            {
+                int s = i + sum, 
+                    neg = s - nums[j], 
+                    pos = s + nums[j];
+                
+                dp[s][j] = 0;
+                
+                if (pos < m)  dp[s][j] += dp[pos][j-1];
+                if (neg >= 0) dp[s][j] += dp[neg][j-1];
+                // cout << endl;
+            }
+        
+        return dp[target + sum][n-1];
+    }
+};
