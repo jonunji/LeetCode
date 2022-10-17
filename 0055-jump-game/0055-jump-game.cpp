d65745d372ed6df5e1153f00e3dfb3d573ebb93d
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-16 22:16:56 -0400

    Time: 177 ms (34.81%), Space: 48.5 MB (43.79%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
+//     DP solution (kinda idk what to call this)
+//     bool canJump(vector<int>& nums) 
+//     {
+//         int n = nums.size(), end = min(nums[0], n-1);
+        
+//         for (int i = 0; i <= end; i++)
+//         {
+//             int pos = min(i + nums[i], n-1);
+//             if (pos == n-1) return true;
+            
+//             end = max(pos, end);
+//         }
+        
+//         return false;
+//     }
+    
     bool canJump(vector<int>& nums) 
     {
-        int n = nums.size(), end = min(nums[0], n-1);
+        int n = nums.size(), end = n-1;
         
-        for (int i = 0; i <= end; i++)
-        {
-            int pos = min(i + nums[i], n-1);
-            if (pos == n-1) return true;
-            
-            end = max(pos, end);
-        }
+        for (int i = n-1; i >= 0; i--)
+            if (i + nums[i] >= end)
+                end = i;
         
-        return false;
+        return end == 0;
     }
 };
