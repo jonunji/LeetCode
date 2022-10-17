7af39641a70bc4c222513abbab7e5e402f7eb459
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-16 22:14:33 -0400

    Time: 197 ms (26.93%), Space: 48.4 MB (77.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,19 +3,15 @@ public:
     bool canJump(vector<int>& nums) 
     {
         int n = nums.size(), end = min(nums[0], n-1);
-        if (n <= 1) return true;
-        
-        bool dp[n];
-        for (int i = 0; i < n; i++) dp[i] = false;
         
         for (int i = 0; i <= end; i++)
         {
             int pos = min(i + nums[i], n-1);
-                
-            dp[pos] = true;
+            if (pos == n-1) return true;
+            
             end = max(pos, end);
         }
         
-        return dp[n-1];
+        return false;
     }
 };
