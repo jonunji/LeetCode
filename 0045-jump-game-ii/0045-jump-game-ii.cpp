29bad67870301ed3c0c9b5f8cc0f560296268530
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-18 13:54:21 -0400

    Time: 44 ms (50.53%), Space: 16.7 MB (51.20%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
+//     DP solution
+//     int jump(vector<int>& nums) 
+//     {
+//         int n = nums.size();
+//         if (n == 1) return 0;
+        
+//         int dp[n];
+//         for (int i = 0; i < n; i++) dp[i] = 1e9;
+//         dp[0] = 0;
+        
+//         for (int i = 0; i < n-1; i++)
+//         {
+//             int end = min(n-1, nums[i] + i);
+
+//             for (int j = i+1; j <= end; j++)
+//                 dp[j] = min(dp[j], dp[i] + 1);
+//         }
+            
+//         return dp[n-1];
+//     }
+    
     int jump(vector<int>& nums) 
     {
         int n = nums.size();
         if (n == 1) return 0;
         
-        int dp[n];
-        for (int i = 0; i < n; i++) dp[i] = 1e9;
-        dp[0] = 0;
-        
-        for (int i = 0; i < n-1; i++)
+        int l = 0, r = 0, res = 0;
+        while (r < n-1)
         {
-            int end = min(n-1, nums[i] + i);
-
-            for (int j = i+1; j <= end; j++)
-                dp[j] = min(dp[j], dp[i] + 1);
-        }
+            int furthest = 0;
+            for (int i = l; i <= r; i++)
+                furthest = max(furthest, nums[i] + i);
             
-        return dp[n-1];
+            l = r+1;
+            r = furthest;
+            res++;
+        }
+        
+        return res;
     }
 };
