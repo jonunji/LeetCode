0b0b01e8df37e1d5fc4913a815d6848de6ebdf5a
jonji <66441843+jonunji@users.noreply.github.com>
2022-10-22 16:50:22 -0400

    Time: 7 ms (63.11%), Space: 6.6 MB (88.75%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -7,16 +7,20 @@ public:
         
         vector<int> res;
         
-        for (int i = n-1; i >= 0; i--) 
-            last[s[i] - 'a'] = max(i, last[s[i] - 'a']);
+        for (int i = n-1; i >= 0; i--)
+        {
+            int letter = s[i] - 'a';
+            last[letter] = max(i, last[letter]);
+        }
         
-        int start = 0, maxPos = 0;
+        
+        int start = 0, end = 0;
         for (int i = 0; i < n; i++)
         {
             int c = last[s[i] - 'a'];
                          
-            maxPos = max(maxPos, c);
-            if (i == maxPos)
+            end = max(end, c);
+            if (i == end)
             {
                 res.push_back(i - start + 1);
                 start = i+1;
