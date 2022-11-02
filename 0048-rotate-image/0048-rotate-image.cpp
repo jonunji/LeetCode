029b37197314c4f023b613ee8b3001649c0b7fd7
jonji <66441843+jonunji@users.noreply.github.com>
2022-11-01 23:39:48 -0400

    Time: 0 ms (100.00%), Space: 7.1 MB (34.01%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -12,12 +12,16 @@ public:
                 int top = left, bottom = right;
                 int topLeft = matrix[top][left + i];
 
+                // top left
                 matrix[top][left + i] = matrix[bottom - i][left];
                 
+                // bottom left
                 matrix[bottom - i][left] = matrix[bottom][right - i];
                 
+                // bottom right
                 matrix[bottom][right - i] = matrix[top + i][right];
                 
+                // top right
                 matrix[top + i][right] = topLeft;
             }
                         
